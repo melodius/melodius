9685f83790dbef0127f172a875f89f127fddaaab
Pramod Kotipalli <pramod.kotipalli@gmail.com>
2016-03-15 22:54:35 -0400

    Created initial User and Room models

--- melodius/harmonious/models.py
+++ melodius/harmonious/models.py
@@ -2,4 +2,28 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """
+    The User object
+    """
+    # The User's username
+    username = models.CharField(max_length=30)
+    # The User's email
+    email = models.EmailField()
+    # The User's password final hash
+    hash = models.CharField(max_length=32)
+    # The User's password salt
+    salt = models.CharField(max_length=32)
+
+
+class Room(models.Model):
+    """
+    The model representing a group of students
+    """
+    # The Room's users who have administrative rights
+    owners = models.ManyToManyField(User)
+    # The Room's users who are just listening
+    listeners = models.ManyToManyField(User)
+    # The preferences for the Room
+    preferences = models.TextField()
